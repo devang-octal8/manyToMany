444f20db78dfd4a4816fba9dc575b8e2def02108
devang <devangd.octal8@gmail.com>
2023-05-31 15:11:42 +0530

    i was adding new comment

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,8 @@ use App\Http\Controllers\authController;
 use App\Http\Controllers\demoController;
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+//Here is all routes
+
 Route::get('/', function () {
     return view('welcome');
 });
